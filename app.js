5ce5a62cd0ec81b7e662f8fc4d96ff1b3a57255b
masudrahman-dev <masudrahman.dev@gmail>
2023-03-20 12:07:15 +0600

    handle error

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const getData = () => {
   fetch('https://restcountries.com/v3.1/all')
     .then((res) => res.json())
     .then((data) => {
-      //   console.log(data);
+      //   spinner(true);
       displayCountries(data);
+      //   spinner(false);
+    })
+    .catch((error) => {
+      console.log('Error fetching data:', error);
     });
 };
-const displayCountries = (data) => {
-  // console.log(data);
-  const countries_container = document.getElementById('countries_container');
-  data.forEach((country) => {
-    // const { flags, capital, name, population, languages, independent, maps } =
-    //   country;
 
+const displayCountries = (data) => {
+  const countries_container = document.getElementById('countries_container');
+  if (!countries_container) {
+    console.log('Container element not found');
+    return;
+  }
+
+  data.forEach((country) => {
     countries_container.innerHTML += countriesHTML(country);
-    // console.log(country);
   });
 };
 
@@ -28,28 +33,35 @@ const countriesHTML = ({
   maps,
 }) => {
   return `
-    <div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="${flags.png}" alt="${flags.alt}" /></figure>
-    <div class="card-body">
-        <h2 class="card-title">
-            ${name.common} 
-        </h2>
-        <div>
-     
-        <ul>
-        <li>capital: ${capital[0]}</li>
-        <li>independent: ${independent}</li>
-        <li>population: ${population}</li>
-        <li>languages: ${Object.values(languages)[0]}</li>
-        </ul>
-        </div>
-        <button class="btn btn-primary"><a href="${
-          maps.googleMaps
-        }">google map</a></button>
-        
+      <div class="card w-96 bg-base-100 shadow-xl">
+      <figure><img src="${flags.png}" alt="${flags.alt}" /></figure>
+      <div class="card-body">
+          <h2 class="card-title">
+              ${name.common} 
+          </h2>
+          <div>
+          <ul>
+           <li>capital: ${capital[0] ? capital[0] : 'not capital'}</li>
+          <li>independent: ${independent}</li>
+          <li>population: ${population}</li>
+          <li>languages: ${Object.values(languages)[0]}</li>
+          </ul>
+          </div>
+          <button class="btn btn-primary"><a href="${
+            maps.googleMaps
+          }">google map</a></button>
+      </div>
     </div>
-</div>
-    `;
+      `;
 };
 
 getData();
+
+function spinner(isSpinner) {
+  if (isSpinner) {
+    // console.log(isSpinner);
+    document.getElementById('spinner').classList.remove('hidden');
+  } else {
+    document.getElementById('spinner').classList.add('hidden');
+  }
+}
